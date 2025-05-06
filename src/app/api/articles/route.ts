f4bb49988733ac41f82b8c8c0b90d0b94d4f2621
abhishek-dagar <dagarabishek584@gmail.com>
2025-05-06 14:22:00 +0530

    fixed build issue

--- src/app/api/articles/route.ts
+++ src/app/api/articles/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { PrismaClient } from "@/generated/prisma";
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 
 const prisma = new PrismaClient();
 
